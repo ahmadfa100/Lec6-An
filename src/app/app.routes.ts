ad572934747a048c620c40b57e7cb51b72a252f0
Ahmad bani Hamad <ahmad100.fa100@gmail.com>
2026-07-14 18:14:46 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,3 @@
 import { Routes } from '@angular/router';
-
+//profile 
 export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Profile } from './profile/profile';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
